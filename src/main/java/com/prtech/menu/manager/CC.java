b732ef8619d0f60202e94577439d24388dbc82d4
a.gjorgjevski <a.gjorgjevski@tibro.tech>
2025-10-01 13:03:32 +0200

    Update constants

--- src/main/java/com/prtech/menu/manager/CC.java
+++ src/main/java/com/prtech/menu/manager/CC.java
@@ -19,11 +19,10 @@ public class CC {
 	public static final String SVAROG_ACL_LBL = "SVAROG_ACL_LBL";
 	public static final String INTERNAL_CAT = "INTERNAL_CAT";
 	public static final String PERUN_MENU_CONF = "PERUN_MENU_CONF";
-	public static final String TABLE_OBJ_ID = "TABLE_OBJ_ID";
 	public static final String TABLE_NAME = "TABLE_NAME";
 	public static final String FIELD_NAME = "FIELD_NAME";
 	public static final String REF_TABLE_NAME = "REF_TABLE_NAME";
-	public static final String REF_FIELD_NAME = "REF_TABLE_NAME";
+	public static final String REF_FIELD_NAME = "REF_FIELD_NAME";
 	public static final String CDL_NAME = "CDL_NAME";
 	public static final String CDL_ITEM_NAME = "CDL_ITEM_NAME";
 
