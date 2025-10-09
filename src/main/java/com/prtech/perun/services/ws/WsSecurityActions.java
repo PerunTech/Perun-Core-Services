d97e190e68fcbb95291e8091fe4ed2277e8afe86
ristepejov <ristepejov@Ritz-E14>
2025-10-09 13:05:46 +0200

    reverse the starts with

--- src/main/java/com/prtech/perun/services/ws/WsSecurityActions.java
+++ src/main/java/com/prtech/perun/services/ws/WsSecurityActions.java
@@ -292,8 +292,8 @@ public class WsSecurityActions {
 				JsonObject jsonUser = samlAttribToJson(at);
 				// ok the user is not registered, so lets take him to the registration form or
 				// just create if its admin
-				if (jsonUser.get("ROLE") != null && jsonUser.get("ROLE").getAsString()
-						.startsWith(svCONST.adminsGroup.getAsString(Sv.GROUP_NAME))) {
+				if (jsonUser.get("ROLE") != null && svCONST.adminsGroup.getAsString(Sv.GROUP_NAME)
+						.startsWith(jsonUser.get("ROLE").getAsString())) {
 					// lets create the admin user and link to the admins group
 					svs.setAutoCommit(false);
 					try (SvLink svl = new SvLink(svs)) {
