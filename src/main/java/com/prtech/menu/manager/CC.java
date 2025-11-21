3efb873726538b3ca908e36de996732bb4ba9cb7
a.gjorgjevski <a.gjorgjevski@tibro.tech>
2025-11-21 12:22:22 +0100

    Add new table for menu placeholder config

--- src/main/java/com/prtech/menu/manager/CC.java
+++ src/main/java/com/prtech/menu/manager/CC.java
@@ -20,12 +20,15 @@ public class CC {
 	public static final String SVAROG_ACL_LBL = "SVAROG_ACL_LBL";
 	public static final String INTERNAL_CAT = "INTERNAL_CAT";
 	public static final String PERUN_MENU_CONF = "PERUN_MENU_CONF";
+	public static final String PERUN_MENU_PH_CONF = "PERUN_MENU_PH_CONF";
 	public static final String TABLE_NAME = "TABLE_NAME";
 	public static final String FIELD_NAME = "FIELD_NAME";
 	public static final String REF_TABLE_NAME = "REF_TABLE_NAME";
 	public static final String REF_FIELD_NAME = "REF_FIELD_NAME";
 	public static final String CDL_NAME = "CDL_NAME";
 	public static final String CDL_ITEM_NAME = "CDL_ITEM_NAME";
+	public static final String PLACEHOLDER_NAME = "PLACEHOLDER_NAME";
+	public static final String SOURCE_FIELD = "SOURCE_FIELD";
 
 	public static final String PARENT_NAME = "PARENT_NAME";
 	public static final String IMPORT_MENU = "IMPORT_MENU";
