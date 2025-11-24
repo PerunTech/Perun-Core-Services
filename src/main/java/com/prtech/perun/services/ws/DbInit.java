72226ef16ab317a569247fe1259560f6ed6e3214
a.gjorgjevski <a.gjorgjevski@tibro.tech>
2025-11-24 12:29:32 +0100

    Add unique constraint

--- src/main/java/com/prtech/perun/services/ws/DbInit.java
+++ src/main/java/com/prtech/perun/services/ws/DbInit.java
@@ -358,6 +358,7 @@ public class DbInit implements IDbInit {
 		dbf2.setDbFieldName(CC.PLACEHOLDER_NAME);
 		dbf2.setDbFieldType(DbFieldType.NVARCHAR);
 		dbf2.setDbFieldSize(50);
+		dbf2.setIsUnique(true);
 		dbf2.setIsNull(false);
 		dbf2.setLabel_code("perun_menu_ph_conf.placeholder_name");
 
