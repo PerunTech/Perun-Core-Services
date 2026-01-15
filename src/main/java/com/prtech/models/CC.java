7f1b2e9f87852fd1714ba20dc0a1fed5f5a0004c
a.gjorgjevski <a.gjorgjevski@tibro.tech>
2026-01-15 12:11:03 +0100

    Implement search functionality inside BaseObjectModel

--- src/main/java/com/prtech/models/CC.java
+++ src/main/java/com/prtech/models/CC.java
@@ -9,10 +9,15 @@ public class CC {
 	public static final String STATUS = "STATUS";
 	public static final String LABEL_CODE = "LABEL_CODE";
 	public static final String FIELD_NAME = "FIELD_NAME";
+	public static final String FIELD_TYPE = "FIELD_TYPE";
 	public static final String PKID = "PKID";
 	public static final String CODE_LIST_ID = "CODE_LIST_ID";
 	public static final String SKIP_CHECK = "SKIP_CHECK";
 	public static final String IS_NULL = "IS_NULL";
 	public static final String VALID = "VALID";
+	public static final String PERCENT_OPERATOR = "%";
+	public static final String ROW_LIMIT = "ROW_LIMIT";
+	public static final String SORT_FIELD = "SORT_FIELD";
+	public static final String SORT_ORDER = "SORT_ORDER";
 
 }
