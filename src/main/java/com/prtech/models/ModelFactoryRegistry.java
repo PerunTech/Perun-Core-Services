1001d36f15ee48e174ff1f6ef99c9aa13e23ebc2
a.gjorgjevski <a.gjorgjevski@tibro.tech>
2026-02-03 12:32:56 +0100

    Add comments

--- src/main/java/com/prtech/models/ModelFactoryRegistry.java
+++ src/main/java/com/prtech/models/ModelFactoryRegistry.java
@@ -2,13 +2,28 @@ package com.prtech.models;
 
 import org.osgi.util.tracker.ServiceTracker;
 
+/**
+ * This registry acts as a central access point for all registered ModelFactory
+ * services in the OSGi environment. ModelFactory services are registered in the
+ * OSGi service registry through the Activator class for each bundle.
+ */
 public class ModelFactoryRegistry {
+	/*
+	 * ServiceTracker instance that tracks all registered ModelFactory services in
+	 * the OSGi service registry.
+	 */
 	private static ServiceTracker<ModelFactory, ModelFactory> tracker;
 
 	public static void setTracker(ServiceTracker<ModelFactory, ModelFactory> t) {
 		tracker = t;
 	}
 
+	/*
+	 * Creates a BaseObjectModel instance for the specified table name. This method
+	 * iterates through all registered ModelFactory services and attempts to create
+	 * a model object for the given table name. The first factory that successfully
+	 * creates a non-null object will have its result returned.
+	 */
 	public static BaseObjectModel createModel(String tableName) {
 		BaseObjectModel obj = null;
 		ModelFactory[] factories = tracker.getServices(new ModelFactory[0]);
