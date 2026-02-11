1bde20315ac084c3b7c2990fa7b2a931787a7906
Zoran Petrovski <z.petrovski@prtech.mk>
2026-02-11 10:33:52 +0100

    Update response

--- src/main/java/com/prtech/perun/services/ws/WsCore.java
+++ src/main/java/com/prtech/perun/services/ws/WsCore.java
@@ -7,12 +7,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.prtech.perun.PerunUtil;
-import com.prtech.svarog.SvCore;
+import com.google.gson.JsonObject;
 import com.prtech.svarog.SvException;
 import com.prtech.svarog.SvReader;
 import com.prtech.svarog_common.DbDataArray;
-import com.prtech.svarog_common.DbDataObject;
+import com.prtech.svarog_common.ResponseHandler;
+import com.prtech.svarog_common.ResponseHandler.MessageType;
 
 public class WsCore {
 
@@ -21,6 +21,7 @@ public class WsCore {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getChildObjectsAsCleanJson(@PathParam("sessionId") String sessionId,
 			@PathParam("parentId") Long parentId, @PathParam("objectName") String objectName) {
+		ResponseHandler jrh = new ResponseHandler();
 		DbDataArray children = new DbDataArray();
 		try (SvReader svr = new SvReader(sessionId)) {
 			if (parentId == null || parentId <= 0)
@@ -30,7 +31,8 @@ public class WsCore {
 			Long objectTypeId = WsReactElements.findTableType(objectName);
 			children = svr.getObjectsByParentId(parentId, objectTypeId, null, 0, 0);
 		} catch (SvException e) {
-			return PerunUtil.handleException(e, "Error fetching children");
+			jrh.create(MessageType.ERROR, "Error fetching children", null, new JsonObject());
+			return Response.ok(jrh.getAll().toString()).build();
 		}
 		return Response.ok(children.toJson().toString()).build();
 	}
@@ -41,10 +43,12 @@ public class WsCore {
 	public Response getObjectsByLinkPerStatuses(@PathParam("sessionId") String sessionId,
 			@PathParam("objectId") Long objectId, @PathParam("table_name") String tableName,
 			@PathParam("linkName") String linkName) {
+		ResponseHandler jrh = new ResponseHandler();
 		try (SvReader svr = new SvReader(sessionId)) {
 			WsReactElements ws = new WsReactElements();
 			if (objectId == null || objectId <= 0)
 				return Response.status(400).entity("{\"error\":\"Invalid objectId\"}").build();
+			jrh.create(MessageType.ERROR, "{\"error\":\"Invalid objectId\"}", null, new JsonObject());
 			if (tableName == null || tableName.isBlank())
 				return Response.status(400).entity("{\"error\":\"Invalid table_name\"}").build();
 			if (linkName == null || linkName.isBlank())
@@ -52,9 +56,9 @@ public class WsCore {
 			DbDataArray linkedObjects = ws.getObjectsByLink(sessionId, objectId, tableName, linkName);
 			return Response.ok(linkedObjects.toJson().toString()).build();
 		} catch (Exception e) {
-			return PerunUtil.handleException(e, "Error getting Objects By Link Per Statuses");
+			jrh.create(MessageType.ERROR, "Error getting Objects By Link Per Statuses", null, new JsonObject());
+			return Response.ok(jrh.getAll().toString()).build();
 		}
 	}
-
 
 }
