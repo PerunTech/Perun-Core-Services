cb8ed904dfbd065dfabf7e8019b270825581ca60
Zoran Petrovski <z.petrovski@prtech.mk>
2026-09-18 11:24:12 +0200

    Fix getTableUISchemaOverride

--- src/main/java/com/prtech/perun/services/ws/WsReactElements.java
+++ src/main/java/com/prtech/perun/services/ws/WsReactElements.java
@@ -5113,7 +5113,7 @@ public class WsReactElements {
 				String tmpField = tempDboField.getVal(Rc.FIELD_NAME).toString();
 				if (processField(tmpField)) {
 					if (tempDboField.getVal(Rc.GUI_METADATA) != null)
-						jsonObj = (JsonObject) tempDboField.getVal(Rc.GUI_METADATA);
+						jsonObj = gson.fromJson(tempDboField.getVal(Rc.GUI_METADATA).toString(), JsonObject.class);
 					if (jsonObj != null && jsonObj.has(Rc.REACT)) {
 						jsonreactGUI = (JsonObject) jsonObj.get(Rc.REACT);
 						if (jsonreactGUI.has(Rc.OVERRIDE_READONLY)
